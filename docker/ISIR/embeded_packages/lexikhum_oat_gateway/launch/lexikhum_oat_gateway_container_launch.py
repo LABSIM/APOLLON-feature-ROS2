52ca3ee154e4869d28b99811534b87727519f020
Nawfel KINANI <nkinani@users.noreply.github.com>
2024-07-15 15:12:20 +0200

    [dev]
    Create lexikhum_oat_gateway_container_launch.py

---
+++ docker/ISIR/embeded_packages/lexikhum_oat_gateway/launch/lexikhum_oat_gateway_container_launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lexikhum_oat_gateway',
+            executable='lexikhum_oat_downstream_gateway',
+            name='downstream'
+        ),
+        Node(
+            package='lexikhum_oat_gateway',
+            executable='lexikhum_oat_upstream_gateway',
+            name='upstream'
+        ),
+        Node(
+            package='ros_tcp_endpoint',
+            executable='default_server_endpoint',
+            name='endpoint',
+            arguments=[
+                '-p','ROS_IP:=0.0.0.0',
+                '-p','ROS_TCP_PORT:=10000'
+            ]
+        )
+    ])
